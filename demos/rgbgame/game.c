d53b89f26580ba9460287de47f12fed33506fe9a
Ulrich Hecht <ulrich.hecht@gmail.com>
2012-10-30 17:11:36 +0100

    rgbgame: fix to build (not tested)

--- demos/rgbgame/game.c
+++ demos/rgbgame/game.c
@@ -1,47 +1,8 @@
 
-//#include "libgame.h"
-
-#define	NULL	(void*)0
-
-extern void **ftab;
+#include <libgame.h>
 
 #define COLOR_ROP_TRANSP	0xcc	// transparency
 #define COLOR_ROP_NOP		0xf0	// nop - just paint
-
-typedef unsigned char		uint8_t;
-typedef unsigned short		uint16_t;
-typedef unsigned long		uint32_t;
-typedef unsigned long long	uint64_t;
-
-typedef struct gfx_rect {
-	uint16_t	x, y;
-	uint16_t	width, height;
-} gfx_rect_t;
-
-typedef struct key_data {
-	uint32_t	key1;
-	uint32_t	key2;
-} key_data_t;
-
-void (*dprintf)(char *fmt, ...);
-
-//int (*MCatchInitGraph)();
-int (*MCatchInitGraph)(void *buffer, int buffsize);
-int (*MCatchSetFrameBuffer)(int width, int height);
-int (*MCatchSetDisplayScreen)(gfx_rect_t *rect);//int with, int height);
-int (*MCatchSetCameraMode)(int mode);
-int (*MCatchSetFGColor)(uint32_t *color);
-uint32_t (*MCatchGetFGColor)();
-int (*MCatchSetColorROP)(uint32_t rop);
-int (*MCatchFillRect)(gfx_rect_t *rect);
-int (*MCatchEnableFeature)(uint32_t feature);
-int (*MCatchFlush)();
-int (*MCatchPaint)();
-
-uint32_t (*NativeGE_getTime)();	// returns system ticks equivalent
-void (*NativeGE_getKeyInput4Ntv)(key_data_t *keys);//uint64_t *keys);
-
-#define	FUNC(n)		*(ftab + (n >> 2))
 
 #define	MAKE_RGB(r, g, b) (r & 0xff) | ((g & 0xff) << 8) | ((b & 0xff) << 16);
 
@@ -53,23 +14,7 @@ int main()
 	uint16_t fbuff[320*240];
 	key_data_t keys, no_keys;
 
-	// setup function pointers
-	dprintf               = FUNC(0x04);
-	MCatchInitGraph              = FUNC(0x38);
-	MCatchSetFrameBuffer   = FUNC(0x90);
-	MCatchSetDisplayScreen= FUNC(0x54);
-	MCatchSetCameraMode      = FUNC(0x8c);
-	MCatchSetColorROP      = FUNC(0x3c);
-	MCatchSetFGColor       = FUNC(0x44);
-	MCatchGetFGColor       = FUNC(0x48);
-	MCatchFillRect          = FUNC(0xc4);
-	MCatchEnableFeature    = FUNC(0x7c);
-	MCatchFlush             = FUNC(0xc);
-	MCatchPaint             = FUNC(0x10);
-	NativeGE_getTime              = FUNC(0x124);
-	NativeGE_getKeyInput4Ntv              = FUNC(0x100);
-
-//	dprintf("Hello World!\n");
+//	diag_printf("Hello World!\n");
 	MCatchInitGraph(fbuff, sizeof(fbuff));
 
 	rect.x = 0;
