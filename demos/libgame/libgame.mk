ad880cd7331a98f9918af08b9c8fed69f4a2e948
Ulrich Hecht <ulrich.hecht@gmail.com>
2012-10-30 17:14:21 +0100

    libgame: remove support for non-newlib build

--- demos/libgame/libgame.mk
+++ demos/libgame/libgame.mk
@@ -17,15 +17,9 @@ RM		= rm -f
 
 LDSCRIPT= $(LIBGAME)/libgame.ld
 
-ifdef NEWLIB
 CFLAGS	+= -O2 -Wall -Wno-format -W -g -c -DHAVE_NEWLIB -I$(LIBGAME) -I$(NEWLIB)/include -I$(3RDPARTY)/include -nostdlib -mcpu=arm926ej-s -msoft-float
 LDFLAGS	= -nostdlib -L$(LIBGAME) -L$(NEWLIB)/lib -L$(3RDPARTY)/lib -march=armv5 -msoft-float -nostartfiles -T$(LDSCRIPT)
 #LIBS	= -lgcc -lgame -lc
-else
-CFLAGS	+= -O2 -c -I$(LIBGAME) -I$(LIBEMU) -nostdlib -march=armv5 -msoft-float
-LDFLAGS	= -nostdlib -L$(LIBGAME) -L$(LIBEMU) -march=armv5 -msoft-float -nostartfiles -T$(LDSCRIPT)
-#LIBS	= -lgcc -lgame -lc
-endif
 
 START_O	= $(LIBGAME)/start.o
 
