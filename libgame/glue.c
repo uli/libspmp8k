d32c98f659413a096e0ebdd6a2cd76b7a3a38074
Ulrich Hecht <ulrich.hecht@gmail.com>
2012-11-01 13:28:35 +0100

    libgame: glue.c: indent properly

--- libgame/glue.c
+++ libgame/glue.c
@@ -3,7 +3,6 @@
  *
  * holds all the stuff to interface newlib to the SPMP8000
  *
- *
  */
 #include "libgame.h"
 
@@ -17,13 +16,12 @@
 #undef errno
 extern int errno;
 
-
-
 /* environ
 **  A pointer to a list of environment variables and their values.
 **  For a minimal environment, this empty list is adequate:
 */
 char *__env[1] = { 0 };
+
 char **environ = __env;
 
 /* _exit
@@ -43,7 +41,8 @@ void _exit(int err)
 */
 int _close(int file)
 {
-	return _ecos_close(file);
+    /* XXX: translate eCos errno (likewise for all other code here) */
+    return _ecos_close(file);
 }
 
 /* execve
@@ -51,12 +50,11 @@ int _close(int file)
 **
 **  Minimal implementation (for a system without processes):
 */
-int _execve(char *name __attribute__((unused)),
-            char **argv __attribute__((unused)),
-            char **env __attribute__((unused)))
+int _execve(char *name __attribute__ ((unused)),
+            char **argv __attribute__ ((unused)), char **env __attribute__ ((unused)))
 {
-	errno=ENOMEM;
-	return -1;
+    errno = ENOMEM;
+    return -1;
 }
 
 /* fork
@@ -67,23 +65,23 @@ int _execve(char *name __attribute__((unused)),
 
 int _fork()
 {
-	errno=EAGAIN;
-	return -1;
+    errno = EAGAIN;
+    return -1;
 }
 
 static void _ecos_stat_to_stat(struct stat *st, struct _ecos_stat *est)
 {
-	memset(st, 0, sizeof(struct stat));
-	st->st_mode = est->st_mode;
-	st->st_ino = est->st_ino;
-	st->st_dev = est->st_dev;
-	st->st_nlink = est->st_nlink;
-	st->st_uid = est->st_uid;
-	st->st_gid = est->st_gid;
-	st->st_size = est->st_size;
-	st->st_atime = est->st_atime;
-	st->st_mtime = est->st_mtime;
-	st->st_ctime = est->st_ctime;
+    memset(st, 0, sizeof(struct stat));
+    st->st_mode = est->st_mode;
+    st->st_ino = est->st_ino;
+    st->st_dev = est->st_dev;
+    st->st_nlink = est->st_nlink;
+    st->st_uid = est->st_uid;
+    st->st_gid = est->st_gid;
+    st->st_size = est->st_size;
+    st->st_atime = est->st_atime;
+    st->st_mtime = est->st_mtime;
+    st->st_ctime = est->st_ctime;
 }
 
 /* fstat
@@ -95,11 +93,11 @@ static void _ecos_stat_to_stat(struct stat *st, struct _ecos_stat *est)
 */
 int _fstat(int file, struct stat *st)
 {
-	struct _ecos_stat est;
-	int ret = _ecos_fstat(file, &est);
-	if (!ret)
-		_ecos_stat_to_stat(st, &est);
-	return ret;
+    struct _ecos_stat est;
+    int ret = _ecos_fstat(file, &est);
+    if (!ret)
+        _ecos_stat_to_stat(st, &est);
+    return ret;
 }
 
 /* getpid
@@ -111,7 +109,7 @@ int _fstat(int file, struct stat *st)
 */
 int _getpid()
 {
-	return 1;
+    return 1;
 }
 
 /* isatty
@@ -120,10 +118,10 @@ int _getpid()
 **  For consistency with the other minimal implementations, which only support output to stdout,
 **  this minimal implementation is suggested:
 */
-int _isatty(int file __attribute__((unused)))
+int _isatty(int file __attribute__ ((unused)))
 {
-//	if (file < 2) return 1;
-	return 0;
+//      if (file < 2) return 1;
+    return 0;
 }
 
 /* kill
@@ -131,10 +129,10 @@ int _isatty(int file __attribute__((unused)))
 **
 **  Minimal implementation:
 */
-int _kill(int pid __attribute__((unused)), int sig __attribute__((unused)))
+int _kill(int pid __attribute__ ((unused)), int sig __attribute__ ((unused)))
 {
-	errno=EINVAL;
-	return(-1);
+    errno = EINVAL;
+    return (-1);
 }
 
 /* link
@@ -142,10 +140,10 @@ int _kill(int pid __attribute__((unused)), int sig __attribute__((unused)))
 **
 **  Minimal implementation:
 */
-int _link(char *old __attribute__((unused)), char *new __attribute__((unused)))
+int _link(char *old __attribute__ ((unused)), char *new __attribute__ ((unused)))
 {
-	errno=EMLINK;
-	return -1;
+    errno = EMLINK;
+    return -1;
 }
 
 /* lseek
@@ -155,7 +153,7 @@ int _link(char *old __attribute__((unused)), char *new __attribute__((unused)))
 */
 int _lseek(int file, int ptr, int dir)
 {
-	return _ecos_lseek(file, ptr, dir);
+    return _ecos_lseek(file, ptr, dir);
 }
 
 /* open
@@ -165,28 +163,35 @@ int _lseek(int file, int ptr, int dir)
 //#ifdef DBG
 //extern int fbuff_printf(char *format, ...);
 //#endif
-int _open(const char *name, int flags, int mode __attribute__((unused)))
+int _open(const char *name, int flags, int mode __attribute__ ((unused)))
 {
-	int _ecos_flags;
-	switch (flags & O_ACCMODE) {
-		case O_RDONLY: _ecos_flags = _ECOS_O_RDONLY; break;
-		case O_RDWR: _ecos_flags = _ECOS_O_RDWR; break;
-		case O_WRONLY: _ecos_flags = _ECOS_O_WRONLY; break;
-		default: return -1;
-	}
-	if (flags & O_APPEND)
-		_ecos_flags |= _ECOS_O_APPEND;
-	if (flags & O_CREAT)
-		_ecos_flags |= _ECOS_O_CREAT;
-	if (flags & O_TRUNC)
-		_ecos_flags |= _ECOS_O_TRUNC;
-	if (flags & O_EXCL)
-		_ecos_flags |= _ECOS_O_EXCL;
-	if (flags & O_SYNC)
-		_ecos_flags |= _ECOS_O_SYNC;
-	if (flags & O_NONBLOCK)
-		_ecos_flags |= _ECOS_O_NONBLOCK;
-	return _ecos_open(name, _ecos_flags, mode);
+    int _ecos_flags;
+    switch (flags & O_ACCMODE) {
+    case O_RDONLY:
+        _ecos_flags = _ECOS_O_RDONLY;
+        break;
+    case O_RDWR:
+        _ecos_flags = _ECOS_O_RDWR;
+        break;
+    case O_WRONLY:
+        _ecos_flags = _ECOS_O_WRONLY;
+        break;
+    default:
+        return -1;
+    }
+    if (flags & O_APPEND)
+        _ecos_flags |= _ECOS_O_APPEND;
+    if (flags & O_CREAT)
+        _ecos_flags |= _ECOS_O_CREAT;
+    if (flags & O_TRUNC)
+        _ecos_flags |= _ECOS_O_TRUNC;
+    if (flags & O_EXCL)
+        _ecos_flags |= _ECOS_O_EXCL;
+    if (flags & O_SYNC)
+        _ecos_flags |= _ECOS_O_SYNC;
+    if (flags & O_NONBLOCK)
+        _ecos_flags |= _ECOS_O_NONBLOCK;
+    return _ecos_open(name, _ecos_flags, mode);
 }
 
 /* read
@@ -194,7 +199,7 @@ int _open(const char *name, int flags, int mode __attribute__((unused)))
 */
 int _read(int file, char *ptr, int len)
 {
-	return _ecos_read(file, ptr, len);
+    return _ecos_read(file, ptr, len);
 }
 
 /* sbrk
@@ -214,21 +219,21 @@ extern caddr_t _sbrk_asm(int incr);
 
 caddr_t _sbrk(int incr)
 {
-	caddr_t ret;
+    caddr_t ret;
 #ifdef DBG
-	dmsg_printf("sbrk(incr(0x%x) 0x%x ", incr, __heap_end_asm);
+    dmsg_printf("sbrk(incr(0x%x) 0x%x ", incr, __heap_end_asm);
 #endif
-	ret = _sbrk_asm((int)incr);
+    ret = _sbrk_asm((int)incr);
 #ifdef DBG
-	dmsg_printf("-> 0x%x) ", __heap_end_asm);
+    dmsg_printf("-> 0x%x) ", __heap_end_asm);
 #endif
-	// safety check
-	if (__heap_end_asm >= RAM_END) {
-		errno = ENOMEM;
-		return (caddr_t)(-1);
-	}
+    // safety check
+    if (__heap_end_asm >= RAM_END) {
+        errno = ENOMEM;
+        return (caddr_t) (-1);
+    }
 
-	return ret;
+    return ret;
 }
 
 /* stat
@@ -236,13 +241,13 @@ caddr_t _sbrk(int incr)
 **
 **  Minimal implementation:
 */
-int _stat(const char *file __attribute__((unused)), struct stat *st)
+int _stat(const char *file __attribute__ ((unused)), struct stat *st)
 {
-	struct _ecos_stat est;
-	int ret = _ecos_stat(file, &est);
-	if (!ret)
-		_ecos_stat_to_stat(st, &est);
-	return ret;
+    struct _ecos_stat est;
+    int ret = _ecos_stat(file, &est);
+    if (!ret)
+        _ecos_stat_to_stat(st, &est);
+    return ret;
 }
 
 /* times
@@ -250,9 +255,9 @@ int _stat(const char *file __attribute__((unused)), struct stat *st)
 **
 **  Minimal implementation:
 */
-clock_t _times(struct tms *buf __attribute__((unused)))
+clock_t _times(struct tms * buf __attribute__ ((unused)))
 {
-	return -1;
+    return -1;
 }
 
 /* unlink
@@ -262,7 +267,7 @@ clock_t _times(struct tms *buf __attribute__((unused)))
 */
 int _unlink(char *name)
 {
-	return _ecos_unlink(name);
+    return _ecos_unlink(name);
 }
 
 /* wait
@@ -270,10 +275,10 @@ int _unlink(char *name)
 **
 **  Minimal implementation:
 */
-int _wait(int *status __attribute__((unused)))
+int _wait(int *status __attribute__ ((unused)))
 {
-	errno=ECHILD;
-	return -1;
+    errno = ECHILD;
+    return -1;
 }
 
 /* write
@@ -289,6 +294,5 @@ int _wait(int *status __attribute__((unused)))
 */
 int _write(int file, char *ptr, int len)
 {
-	return _ecos_write(file, ptr, len);
+    return _ecos_write(file, ptr, len);
 }
-
