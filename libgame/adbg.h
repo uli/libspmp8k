7398417b3cc7fc708174e0ee8c92b707e27a961b
Ulrich Hecht <ulrich.hecht@gmail.com>
2012-11-29 10:41:26 +0100

    adbg: make header file C++ compatible

--- libgame/adbg.h
+++ libgame/adbg.h
@@ -1,4 +1,12 @@
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 int adbg_init(void);
 void adbg_fini(void);
 void adbg_puts(const char *);
 int adbg_printf(const char *fmt, ...);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
